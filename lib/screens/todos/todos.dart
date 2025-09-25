e84eaaf16c840f6db813ada8e6bbc8781eee3b11
Marie <mje.espinosa@gmail.com>
2025-09-25 14:50:54 +0200

    input to add todo

--- lib/screens/todos/todos.dart
+++ lib/screens/todos/todos.dart
@@ -22,18 +22,58 @@ class _TodosPageState extends State<TodosPage> {
     ListItem(title: 'Item 3'),
   ];
 
+  String _newItemTitle = '';
+
   void _setAsDone(int index, bool? value) {
     setState(() {
       items[index].isDone = value ?? false;
     });
   }
 
+  void _addItem(String title) {
+    if (title.trim().isEmpty) return; // ignore si vide
+    setState(() {
+      items.add(ListItem(title: title));
+      _newItemTitle = '';
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Todos'),
+      ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Enter your todo',
+                    ),
+                    onChanged: (text) {
+                      setState(() {
+                        _newItemTitle = text;
+                      });
+                    },
+                  ),
+                ),
+                const SizedBox(width: 8),
+                ElevatedButton(
+                  onPressed: () {
+                    _addItem(_newItemTitle);
+                  },
+                  child: const Text("Add"),
+                ),
+              ],
+            ),
+          ),
           Expanded(
             child: ListView.builder(
               padding: const EdgeInsets.all(8),
